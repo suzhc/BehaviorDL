363a8b7f24586c7e854deaf395e3d114c8e4e4d9
xuxiaoooo <1078446778@qq.com>
2023-09-08 08:46:27 +0800

    add build model and get data func

--- exp_heartbeat.py
+++ exp_heartbeat.py
@@ -26,11 +26,12 @@ class args:
     factor: float = 1
     n_heads: int = 8
     enc_in: int = 7
+    pred_len: int = 0
 args.data_path = '/home/user/suzhao/BehaviorDL/dataset/Huawei'
 args.label_flag = 'emotion'
 args.epochs = 10
 args.seq_len = 1441
-args.d_model = 64
+args.d_model = 128
 args.dropout = 0.6
 args.e_layers = 3
 args.d_ff = 128
@@ -44,16 +45,45 @@ args.enc_in = 7
 
 data_set = UEAloader('/home/user/suzhao/BehaviorDL/dataset/Heartbeat')
 
-train_size = int(0.8 * len(data_set))
-test_size = len(data_set) - train_size
-train_dataset, test_dataset = random_split(data_set, [train_size, test_size])
+# train_size = int(0.8 * len(data_set))
+# test_size = len(data_set) - train_size
+# train_dataset, test_dataset = random_split(data_set, [train_size, test_size])
+
+# train_loader = DataLoader(
+#     train_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn)
+# test_loader = DataLoader(
+#     test_dataset, batch_size=16, shuffle=False, collate_fn=collate_fn)
 
 train_loader = DataLoader(
-    train_dataset, batch_size=10, shuffle=True, collate_fn=collate_fn)
-test_loader = DataLoader(
-    test_dataset, batch_size=10, shuffle=False, collate_fn=collate_fn)
+    data_set, batch_size=16, shuffle=True, collate_fn=collate_fn)
 
-model = PatchTST.Model(args).float()
+def get_data(flag='TRAIN'):
+    if flag == 'TRAIN':
+        train_dataset = UEAloader('/home/user/suzhao/BehaviorDL/dataset/Heartbeat', flag='TRAIN')
+        train_loader = DataLoader(
+            train_dataset, batch_size=16, shuffle=True, collate_fn=collate_fn)
+        return train_dataset, train_loader
+    elif flag == 'TEST':
+        test_dataset = UEAloader('/home/user/suzhao/BehaviorDL/dataset/Heartbeat', flag='TEST')
+        test_loader = DataLoader(
+            test_dataset, batch_size=16, shuffle=False, collate_fn=collate_fn)
+        return test_dataset, test_loader
+    else:
+        raise NotImplementedError
+
+def build_model():
+    # model input depends on data
+    train_data, train_loader = get_data(flag='TRAIN')
+    test_data, test_loader = get_data(flag='TEST')
+    args.seq_len = max(train_data.max_seq_len, test_data.max_seq_len)
+    args.pred_len = 0
+    args.enc_in = train_data.feature_df.shape[1]
+    args.num_class = len(train_data.class_names)
+    # model init
+    model = PatchTST.Model(args).float()
+    return model
+
+model = build_model()
 
 train_steps = len(train_loader)
 model_optim = optim.Adam(model.parameters(), lr=0.02)
