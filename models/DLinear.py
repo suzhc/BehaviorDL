5b8478f3c670963218f7ba6d0b1c75e29e6a12cd
xuxiaoooo <1078446778@qq.com>
2023-10-30 20:05:44 +0800

    add DLinear model

---
+++ models/DLinear.py
@@ -0,0 +1,17 @@
+import torch
+import torch.nn as nn
+
+class DLinear(nn.Module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(DLinear, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, output_dim)
+        self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.softmax(out)
+        return out
